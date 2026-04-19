ad0945f7fb76c2ca42890d29479d8cc175108363
Natanael <151780037+pronatan@users.noreply.github.com>
2026-04-19 00:10:11 -0300

    chore: remove test-selenium-real.sh

--- test-selenium-real.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Testa com um reel público real do Instagram
-URL="https://www.instagram.com/reel/C9example/"
-mkdir -p /tmp/test-dl
-
-sudo -u www-data HOME=/tmp /opt/selenium-env/bin/python3 \
-    /var/www/baixar-media-player/instagram_selenium.py \
-    "$URL" /tmp/test-dl/ 2>&1 | grep -E '^\[selenium\]|\{' | tail -20
